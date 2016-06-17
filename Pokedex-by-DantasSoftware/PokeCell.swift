f2c100479d6e9488764c6cdb1aa89602bfe1329a
Fabio Dantas <Dantasireland@Fabios-iMac.local>
2016-06-18 00:13:37 +0100

    Main UI finished, Collection Populated with images

--- Pokedex-by-DantasSoftware/PokeCell.swift
+++ Pokedex-by-DantasSoftware/PokeCell.swift
@@ -13,6 +13,12 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 5.0
+        
+    }
+    
     func configureCell(pokemon: Pokemon) {
         
         self.pokemon = pokemon
